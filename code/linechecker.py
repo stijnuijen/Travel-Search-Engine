8dbe1ef6f75975aaa737b35689ad57cc9a8fc022
Jesse Haenen <jessehaenen@gmail.com>
2019-02-22 13:05:13 +0100

    folders

--- code/linechecker.py
+++ code/linechecker.py
@@ -1,4 +1,6 @@
 with open('url_list_large.txt', 'r') as f:
+
     directory = 'lonelyplanet'
+    
     sumy = sum(1 for line in f if directory in line)
     print("{} page count:{}".format(directory, sumy))
